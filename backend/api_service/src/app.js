fd0392e688fd9debebe23cde697d9857891da14d
Chaitanya Sai Meka <marvelchaitanya@gmail.com>
2025-10-19 17:05:15 +0530

    created basic server

---
+++ backend/api_service/src/app.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const app =express();
+const port = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Voyagr API is running');
+});
+
+app.listen(port, () => {
+  console.log(`Voyagr API is running on port ${port}`);
+});
+
+export default app;
