7db699679ec9b0fdaa43465d9d49141687c0384c
Chaitanya Sai Meka <marvelchaitanya@gmail.com>
2025-11-04 17:55:29 +0530

    delleted route_services

--- backend/routes_service/src/routes/calculation.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { handleCalculation } from '../controllers/calculation.controller.js';
-
-const router = Router();
-
-router.post('/calculate', handleCalculation);
-
-export default router;
--- backend/routes_service/src/routes/map.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { getMapLines } from '../controllers/map.controller.js';
-
-const router = Router();
-
-router.get('/map-lines', getMapLines);
-
-export default router;
